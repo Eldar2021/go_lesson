1b534c045b90be444f2bdaed1d46e2c5617a1021
Eldar2021 <eldiiaralmazbekov@gmail.com>
2023-08-05 14:05:00 +0600

    hi

---
+++ go02/main.go
@@ -0,0 +1,36 @@
+package main
+
+/*
+Integer Türler
+Öncelikle tüm integer türleri bir görelim;
+int, int8, int16, int32, int64
+uint, uint8, uint16, uint32, uint64, uintptr
+
+int8  -> 8-bit integers (-128 to 127)
+int16 -> 16-bit integers (-32768 to 32767)
+int32 -> 32-bit integers (-2147483648 to 2147483647)
+int64 -> 64-bit integers (-9223372036854775808 to 9223372036854775807)
+
+uint -> 32 or 64 bits
+int -> uint (32 or 64 bits)
+
+uint8 -> 0-255
+uint16 -> 0-65535
+uint32 -> 0-42967295
+uint64 -> 0-18446744073709551615
+
+Integer sayısal veri tipleri içierisinden bahsedebileceğimiz son tipler ise int ve uint.
+Int ve uint veri tipleri kullanmış olduğumuz işletim sistemi 32bit ise 32bit değer
+alırlar, 64bit ise 64bit değer alırlar. Sayısal bir değer atanacağı zaman en çok
+kullanılan veri tipleridir. Genellikle int daha çok kullanılır. Eğer çok meşakkatli
+bir program yazmayacaksanız int kullanmanız önerilir.
+
+Byte Veri Tipi: uint8 ile aynıdır.
+Rune: int32 ile aynıdır. Unicode karakter kodlarını ifade eder.
+*/
+
+/*
+Float türleri integer türlerden farklı olarak küsüratlı sayıları tutar. Örnek: 3.14
+
+
+*/
